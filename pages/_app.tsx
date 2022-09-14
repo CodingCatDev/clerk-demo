d0ea631798b393aabd72d631a02cfb736d8ef7be
Alex Patterson <alex@codingcat.dev>
2022-09-14 14:48:03 -0400

    frontend

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,40 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignIn,
+  UserButton,
+} from "@clerk/nextjs";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ClerkProvider
+      appearance={{
+        signIn: {
+          elements: {
+            formButtonPrimary: {
+              color: "white",
+              backgroundColor: "#5e1286",
+            },
+          },
+        },
+        variables: {
+          colorPrimary: "red",
+          colorBackground: "yellow",
+        },
+      }}
+    >
+      <SignedIn>
+        <UserButton />
+        <Component {...pageProps} />
+      </SignedIn>
+      <SignedOut>
+        <SignIn />
+      </SignedOut>
+    </ClerkProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
